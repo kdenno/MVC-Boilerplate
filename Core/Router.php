b0030db1b8a194056cc427fd3fccfa247ad710c8
kdenno <kabuyedennis3@gmail.com>
2020-03-05 16:02:37 +0300

    create routing engine using regex

--- Core/Router.php
+++ Core/Router.php
@@ -3,6 +3,7 @@ class Router
 {
   // create a routing table
   protected $routes = [];
+  protected $params = [];
   /**
    * Add a route to the routing table
    * 
@@ -11,8 +12,17 @@ class Router
    * 
    * @return void
    */
-  public function add($route, $params)
+  public function add($route, $params=[])
   {
+    // convert the route to regular expressions, escape the forward slashes
+    $route = preg_replace('/\//', '\\/', $route);
+    // convert variables e.g {controllers}
+    $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+    // convert variables with custom regular expressions e.g {id:\d+}
+    $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+    // add start and end delimiters and case isensitive flag
+    $route = '/^' . $route . '$/i';
+
     $this->routes[$route] = $params;
   }
 
@@ -25,5 +35,44 @@ class Router
   public function getRoutes()
   {
     return $this->routes;
+  }
+
+  /**
+   * match the route to the routes in the routing table, setting the $params property if the route is found
+   *
+   * @param  string $url the route URL
+   *
+   * @return boolean true if the match is found
+   */
+  public function match($url)
+  {
+    /*
+    foreach ($this->routes as $route => $params) {
+      if ($url == $route) {
+        $this->params = $params;
+        return true;
+      }
+    }
+    */
+    // $reg_exp = '/^(?P<controller>[a-z-]+)\/(?<action>[a-z-]+)$/';
+    foreach($this->routes as $route => $params) {
+    if (preg_match($route, $url, $matches)) {
+      foreach ($matches as $key => $value) {
+        if (is_string($key)) {
+          $params[$key] = $value;
+        }
+      }
+    }
+   
+      $this->params = $params;
+      return true;
+    }
+    return false;
+  }
+
+
+  public function getParams()
+  {
+    return $this->params;
   }
 }
